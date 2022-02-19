f78e8b5411063303657a2eb84f19ef4a24f3c0fd
Simon Geilfus <simongeilfus@users.noreply.github.com>
2022-02-19 11:50:11 +0100

    Initial commit

---
+++ include/liveplusplus/liveplusplus.h
@@ -0,0 +1,132 @@
+﻿/*
+Copyright 2020 Simon Geilfus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES 
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+#pragma once
+
+#include <filesystem>
+#include <functional>
+#include <optional>
+
+#if ! defined( LPP_PATH )
+	#define LPP_PATH "D:\\LivePP"
+#endif
+
+class LivePlusPlus {
+public:
+	enum class WindowMode { Normal, Minimized, Maximized };
+	enum class FocusMode { OnError, OnSuccess, OnShortcut, Never };
+
+	class Settings {
+	public:		
+		//! Sets whether the exception handler / error recovery system should be installed
+		Settings& installExceptionHandler( bool exceptionHandler = true ) { mInstallExceptionHandler = exceptionHandler; return *this; }
+		//! Sets whether Live++ starts minimized, maximized, or normal.
+		Settings& initialWindowMode( WindowMode mode ) { mInitialWindowMode = mode; return *this; }
+		//! Sets if and under what circumstances Live++ should get focus upon re-compiling.
+		Settings& receiveFocusOnRecompile( FocusMode mode ) { mReceiveFocusOnRecompile = mode; return *this; }
+		//! When continuous compilation is turned on, Live++ waits for change notifications in the given directory (and its sub-directories) and automatically compiles any changes once the timeout has expired.
+		Settings& enableContinuousCompilation( bool enabled = true ) { mEnableContinuousCompilation = enabled; return *this; }
+		//! Directory to watch 	Lets you set the directory that is being watched for changes when using continuous compilation.
+		Settings& continuousCompilationPath( const std::filesystem::path &path ) { mContinuousCompilationPath = path; return *this; }
+		//! When listening for change notifications, Live++ will wait for subsequent changes until the timeout is reached.
+		Settings& continuousCompilationTimeout( int32_t ms ) { mContinuousCompilationTimeout = ms; return *this; }
+		//! Enables restart from the Lpp App
+		Settings& enableRestart( bool enabled = true ) { mEnableRestart = enabled; if( enabled ) return enableMultiProcessEditing(); else return *this; }
+		//! By default, you can launch several executables and register them at the same process group, but only if no patches have been applied yet. When this setting is enabled, registered modules will automatically load all available patches when registering with a process group. 
+		Settings& enableMultiProcessEditing( bool enabled = true ) { mEnableMultiProcessEditing = enabled; return *this; }
+		// Specifies additional options passed to the compiler when creating a patch
+		Settings& additionalCompilerOptions( const std::wstring &options ) { mAdditionalCompilerOptions = options; return *this; }
+		Settings();
+	protected:
+
+		void apply( void* lppModule ) const;
+		bool mInstallExceptionHandler;
+		bool mEnableRestart;
+		std::optional<bool> mEnableMultiProcessEditing;
+		std::optional<WindowMode> mInitialWindowMode;
+		std::optional<FocusMode> mReceiveFocusOnRecompile;
+		std::optional<bool> mEnableContinuousCompilation;
+		std::filesystem::path mContinuousCompilationPath;
+		int32_t mContinuousCompilationTimeout;
+		std::wstring mAdditionalCompilerOptions;
+		friend LivePlusPlus;
+	};
+
+	//! Returns the global instance
+	static LivePlusPlus& instance();
+	//! Initialize the global instance
+	static void initialize( const Settings &settings = Settings(), const std::filesystem::path &lppPath = LPP_PATH );
+	//! Specifies when the the syncing point should happen
+	static void syncPoint();
+	
+	//! Used to keep track of the lifetime of callbacks
+	class Connection {
+	public:
+		Connection() = default;
+		Connection( uint32_t id, class LivePlusPlus* parent );
+		Connection( const Connection &other ) = delete;
+		Connection& operator=( const Connection &other ) = delete;
+		//! Removes this callback from Lpp
+		void disconnect();
+		//! Returns the unique id associated with this callback
+		uint32_t getId() const;
+		//! Returns the FileWatcher that issued this Connection
+		class LivePlusPlus* getParent() const;
+	protected:
+		uint32_t			mId;
+		class LivePlusPlus*	mParent;
+		friend class LivePlusPlus;
+	};
+
+	//! Raii version of Connection 
+	class ScopedConnection : public Connection {
+	public:
+		ScopedConnection() : Connection() {}
+		ScopedConnection( const Connection &other );
+		ScopedConnection& operator=( const Connection &other );
+		~ScopedConnection();
+	};
+	
+	//! Adds a function to be called before a live++ patch is applied
+	Connection addPrePatchFn( const std::function<void()> &fn );
+	//! Adds a function to be called after a live++ patch is applied
+	Connection addPostPatchFn( const std::function<void()> &fn );
+	//! Removes any target matching the Connection
+	void removeFn( uint32_t connectionId );
+	//! Removes any target matching the Connection
+	void removePrePatchFn( const Connection &connection );
+	//! Removes any target matching the Connection
+	void removePostPatchFn( const Connection &connection );
+	
+	LivePlusPlus();
+	LivePlusPlus( const Settings &settings, const std::filesystem::path &lppPath = LPP_PATH );
+	
+	void emitPostPatch();
+	void emitPrePatch();
+private:
+	void initializeLpp( const Settings &settings, const std::filesystem::path &lppPath );
+	void* mModule;
+	using Callback = std::pair<uint32_t,std::function<void()>>;
+	std::vector<Callback> mPostPatchFunctions;
+	std::vector<Callback> mPrePatchFunctions;
+	uint32_t mFunctionsIds;
+};
+
+#if ! defined( LIVEPLUSPLUS_DISABLE_ALIAS )
+	using Lpp = LivePlusPlus;
+#endif
---
+++ src/liveplusplus/liveplusplus.cpp
@@ -0,0 +1,239 @@
+﻿/*
+Copyright 2020 Simon Geilfus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial 
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES 
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+#include "liveplusplus/liveplusplus.h"
+
+#if defined(_WIN32)
+	#define NOMINMAX
+	#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <Windows.h>
+#include <LPP_API.h>
+//#include <LPP_ForceLinkStaticRuntime.h>
+
+LivePlusPlus::LivePlusPlus()
+	: mFunctionsIds( 0 ),
+	mModule( nullptr )
+{
+}
+
+LivePlusPlus::LivePlusPlus( const Settings &settings, const std::filesystem::path &lppPath )
+	: LivePlusPlus()
+{
+	initializeLpp( settings, lppPath );
+}
+
+LivePlusPlus& LivePlusPlus::instance()
+{
+	static LivePlusPlus inst;
+	return inst;
+}
+
+LivePlusPlus::Connection::Connection( uint32_t id, LivePlusPlus* parent )
+	: mId( id ), mParent( parent )
+{
+}
+
+void LivePlusPlus::Connection::disconnect()
+{
+	if( mParent ) {
+		mParent->removeFn( mId );
+	}
+}
+
+uint32_t LivePlusPlus::Connection::getId() const 
+{ 
+	return mId; 
+}
+
+LivePlusPlus* LivePlusPlus::Connection::getParent() const 
+{ 
+	return mParent; 
+}
+
+LivePlusPlus::ScopedConnection::ScopedConnection( const Connection &other )
+{
+	mId = other.getId();
+	mParent = other.getParent();
+}
+
+LivePlusPlus::ScopedConnection& LivePlusPlus::ScopedConnection::operator=( const Connection &other )
+{
+	mId = other.getId();
+	mParent = other.getParent();
+	return *this;
+}
+
+LivePlusPlus::ScopedConnection::~ScopedConnection() 
+{ 
+	disconnect(); 
+}
+	
+LivePlusPlus::Connection LivePlusPlus::addPrePatchFn( const std::function<void()> &fn )
+{
+	uint32_t id = mFunctionsIds++;
+	mPrePatchFunctions.push_back( { id, fn } );
+	return Connection( id, this );
+}
+
+LivePlusPlus::Connection LivePlusPlus::addPostPatchFn( const std::function<void()> &fn )
+{
+	uint32_t id = mFunctionsIds++;
+	mPostPatchFunctions.push_back( { id, fn } );
+	return Connection( id, this );
+}
+
+void LivePlusPlus::removeFn( uint32_t connectionId )
+{
+	for( auto it = mPostPatchFunctions.begin(); it != mPostPatchFunctions.end(); ) {
+		if( it->first == connectionId ) {
+			it = mPostPatchFunctions.erase( it );
+		}
+		else {
+			++it;
+		}
+	}
+	for( auto it = mPrePatchFunctions.begin(); it != mPrePatchFunctions.end(); ) {
+		if( it->first == connectionId ) {
+			it = mPrePatchFunctions.erase( it );
+		}
+		else {
+			++it;
+		}
+	}
+}
+
+void LivePlusPlus::removePrePatchFn( const Connection &connection )
+{
+	removeFn( connection.getId() );
+}
+
+void LivePlusPlus::removePostPatchFn( const Connection &connection )
+{
+	removeFn( connection.getId() );
+}
+
+void LivePlusPlus::emitPostPatch()
+{
+	for( const auto &fn : mPostPatchFunctions ) {
+		fn.second();
+	}
+}
+
+void LivePlusPlus::emitPrePatch()
+{
+	for( const auto &fn : mPrePatchFunctions ) {
+		fn.second();
+	}
+}
+
+void postPatch()
+{
+	LivePlusPlus::instance().emitPostPatch();
+}
+
+LPP_POSTPATCH_HOOK( postPatch );
+
+void prePatch()
+{
+	LivePlusPlus::instance().emitPrePatch();
+}
+
+LPP_PREPATCH_HOOK( prePatch );
+
+void LivePlusPlus::initialize( const LivePlusPlus::Settings &settings, const std::filesystem::path &lppPath )
+{
+	instance().initializeLpp( settings, lppPath );
+}
+
+void LivePlusPlus::initializeLpp( const LivePlusPlus::Settings &settings, const std::filesystem::path &lppPath )
+{
+	mModule = lpp::lppLoadAndRegister( lppPath.wstring().c_str(), "Live++ProcessGroup" );
+	
+	if( ! mModule ) {
+		throw std::exception( ( "Failed to load Live++ library at " + lppPath.string() ).c_str() );
+	}
+	lpp::lppEnableAllCallingModulesAsync( static_cast<HMODULE>( mModule ) );
+
+	settings.apply( mModule );
+}
+
+void LivePlusPlus::syncPoint()
+{
+	lpp::lppSyncPoint( static_cast<HMODULE>( instance().mModule ) );
+}
+
+LivePlusPlus::Settings::Settings() 
+	: mInstallExceptionHandler( false ),
+	mInitialWindowMode( WindowMode::Minimized ),
+	mReceiveFocusOnRecompile( FocusMode::OnError ),
+	mEnableContinuousCompilation( true ),
+	mContinuousCompilationTimeout( -1 ),
+	mEnableRestart( false ),
+	mEnableMultiProcessEditing( false )
+{
+}
+
+namespace {
+	const wchar_t* getWindowModeString( LivePlusPlus::WindowMode mode )
+	{
+		if( mode == LivePlusPlus::WindowMode::Maximized ) return L"Maximized";
+		else if( mode == LivePlusPlus::WindowMode::Minimized ) return L"Minimized";
+		else if( mode == LivePlusPlus::WindowMode::Normal ) return L"Normal";
+		return L"";
+	}
+
+	const wchar_t* getFocusModeString( LivePlusPlus::FocusMode mode )
+	{
+		if( mode == LivePlusPlus::FocusMode::Never ) return L"Never";
+		else if( mode == LivePlusPlus::FocusMode::OnError ) return L"On error";
+		else if( mode == LivePlusPlus::FocusMode::OnShortcut ) return L"On shortcut";
+		else if( mode == LivePlusPlus::FocusMode::OnSuccess ) return L"On success";
+		return L"";
+	}
+}
+
+void LivePlusPlus::Settings::apply( void* lppModule ) const
+{
+	HMODULE lppHModule = static_cast<HMODULE>( lppModule );
+	if( mInstallExceptionHandler ) {
+		lpp::lppInstallExceptionHandler( lppHModule );
+	}
+	if( mInitialWindowMode ) {
+		lpp::lppApplySettingString( lppHModule, "initial_window_mode", getWindowModeString( mInitialWindowMode.value() ) );
+	}
+	if( mReceiveFocusOnRecompile ) {
+		lpp::lppApplySettingString( lppHModule, "receive_focus_on_recompile", getFocusModeString( mReceiveFocusOnRecompile.value() ) );
+	}
+	if( mEnableContinuousCompilation ) {
+		lpp::lppApplySettingBool( lppHModule, "continuous_compilation_enabled", mEnableContinuousCompilation.value() );
+	}
+	if( !mContinuousCompilationPath.empty() ) {
+		lpp::lppApplySettingString( lppHModule, "continuous_compilation_path", mContinuousCompilationPath.wstring().c_str() );
+	}
+	if( mContinuousCompilationTimeout != -1 ) {
+		lpp::lppApplySettingInt( lppHModule, "continuous_compilation_timeout", mContinuousCompilationTimeout );
+	}
+	if( mEnableMultiProcessEditing ) {
+		lpp::lppApplySettingBool( lppHModule, "install_compiled_patches_multi_process", mEnableMultiProcessEditing.value() );
+	}
+	if( !mAdditionalCompilerOptions.empty() ) {
+		lpp::lppApplySettingString( lppHModule, "additional_compiler_options", mAdditionalCompilerOptions.c_str() );
+	}
+}
